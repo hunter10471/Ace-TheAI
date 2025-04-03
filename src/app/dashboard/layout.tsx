c807f39ed8aa5300f43f7dce1288a87d4787a240
hunter10471 <rafayzia10@gmail.com>
2025-04-03 13:01:55 +0500

    feat(husky): add husky to the project

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <main className="flex gap-5 lg:gap-10 py-5 px-5 lg:px-10 bg-text justify-center h-screen">
       <Sidebar />
-      <div className="bg-offWhite rounded-xl w-full p-8 max-w-screen-lg">
+      <div className="bg-offWhite rounded-xl w-full p-8 max-w-screen-lg h-full overflow-y-auto">
         {children}
       </div>
     </main>
